7b98278597718767ef100e0e10228a70fb62a15c
AngelRomero26 <a3523110195@uttehuacan.edu.mx>
2025-01-19 17:07:35 -0600

    fourth commit

--- App.js
+++ App.js
@@ -1,62 +1,75 @@
-import { StyleSheet, Text, View, SafeAreaView, TextInput, FlatList, Image } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, TextInput, FlatList, Image, StatusBar } from "react-native";
 import React from "react";
 import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
-import {InstantSearch, connectCurrentRefinements, connectSearchBox} from 'react-instantsearch-native';
+import {connectSearchBox, connectInfiniteHits} from 'react-instantsearch-native';
+import {InstantSearch} from 'react-instantsearch-native';
 
 const App = () => {
 
   const searchClient = instantMeiliSearch(
     '172.233.129.212',
-    '9674910d2440954f136b0e1537007301972088cd71ca00c44008f4a9a024'
+    '2a5fb1e956eccf0fd9e1555842e4b6bd96fe244fabfe83b9e15a1487816d'
   );
 
   const SearchBox = ({ refine, currentRefinement}) => {
     return(
-        <View style ={styles.container}>
-        <TextInput
-            style = {styles.input}
-            onChangeText={ value => refine(value)}
-            value ={currentRefinement}
-            placeholder=" Escribe aqui"
-        />
+        <View style ={styles.searchBarContainer}>
+            <View style ={styles.searchBar}>
+                <TextInput
+                    style = {styles.input}
+                    onChangeText={ value => refine(value)}
+                    value ={currentRefinement}
+                    placeholder=" Escribe aqui"
+                />
+            </View>
         </View>
     )
   }
 
   const CustomSearchBox = connectSearchBox(SearchBox);
 
-  const InfiniteHits = ({}) => {
+  const InfiniteHits = ({hits}) => {
     return(
         <FlatList
             data = {hits}
             keyExtractor={item => item.id}
             renderItem={({item}) => (
-                <View style ={styles.item}>
-                    <Image
-                        source = {{
-                            uri: item.image,
-                        }}
-                        style = {{
-                            width: 100,
-                            height: 50,
-                        }}
-                        resizeMode = "contain"
-                    />
-                    <Text> {item.name} </Text>
+                <View style ={styles.InfiniteHitsContainer}>
+                    <View style ={styles.InfiniteHits}>
+                        <Image
+                            source = {{
+                                uri: item.image,
+                            }}
+                            style = {{
+                                width: 100,
+                                height: 50,
+                            }}
+                            resizeMode = "contain"
+                        />
+                        <View style ={styles.TextContainer}>
+                            <Text style = { styles.namedText}> {item.name} </Text>
+                        </View>
+                    </View>
                 </View>
             )}
-        ></FlatList>
+        />
     );
   };
 
-    return(
-        <SafeAreaView>
+  const CustomInfiniteHits = connectInfiniteHits(InfiniteHits)
 
-          <InstantSearch
-            indexName ="movies" 
-            searchClient = {searchClient}>
-            <CustomSearchBox/>
-          </InstantSearch>
+    return(
+        <SafeAreaView style ={styles.container}>
+            <StatusBar
+                barStyle="light-content"
+                //hidden={false}
+                backgroundColor="00BCD4"
+                translucent = {true}
+            />
+            <InstantSearch indexName ="movies" searchClient = {searchClient}>
+                <CustomSearchBox/>
+                <CustomInfiniteHits/>
+            </InstantSearch>
             <Text>App</Text>
         </SafeAreaView>
     );
@@ -64,4 +77,40 @@ const App = () => {
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:"#300B58",
+    },
+    searchBarContainer:{
+        padding:20,
+    },
+    searchBar:{
+        backgroundColor: "#fff",
+        height:50,
+        borderRadius: 10,
+        justifyContent:'center',
+        //align:'center'
+        paddingHorizontal:10
+    }, 
+    InfiniteHitsContainer:{
+        padding:20,
+    },
+    InfiniteHits:{
+        backgroundColor: "#fff",
+        height:80,
+        borderRadius: 10,
+        //justifyContent:'center',
+        alignItems:'center',
+        paddingHorizontal:10,
+        flexDirection:"row"
+    },
+    TextContainer:{
+        padding:20,   
+    },
+    namedText:{
+        fontSize:18,
+        fontWeight:'bold',
+        width:200,
+        height:50,
+    }
+});
