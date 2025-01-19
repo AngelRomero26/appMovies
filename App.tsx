6c5b26e8512d4876f6a97794032cb556e7eba20c
AngelRomero26 <a3523110195@uttehuacan.edu.mx>
2025-01-19 12:02:19 -0600

    second commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import React from "react";
+import { instantMeiliSearch } from "@meilisearch/instant-meilisearch";
 
 const App = () => {
     return(
